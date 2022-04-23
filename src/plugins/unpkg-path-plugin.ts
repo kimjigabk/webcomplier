2e8d462d3abcc0e895f352b67f12af7f93035a08
kimji <kimjigabk@gmail.com>
2022-04-23 00:07:13 -0400

    add comment

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -7,14 +7,23 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
+        //Figure out where index.js is stored
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a',
-          };
         }
+
+        // Figure out where the requested file is (from import/require)
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -28,7 +37,10 @@ export const unpkgPathPlugin = () => {
               console.log(message);
             `,
           };
-        }
+        } // parse the index.js file, find any import/require/exports
+        // args.path is updated
+
+        //Attempt to load that file
         const { data } = await axios.get(args.path); // content of index.js file
         return {
           loader: 'jsx',
