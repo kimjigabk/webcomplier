0923fd052ba43e0951a0e1ffc0a8b71587531d8b
kimji <kimjigabk@gmail.com>
2022-04-23 00:00:28 -0400

    Attempt to load and build tiny-test-pkg from unpkg.com

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
@@ -6,7 +7,14 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a',
+          };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +24,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
         }
+        const { data } = await axios.get(args.path); // content of index.js file
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
