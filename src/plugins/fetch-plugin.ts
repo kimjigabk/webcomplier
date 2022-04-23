b263e6bcb0df3d473ecceb48b5b0d00ac4639b17
kimji <kimjigabk@gmail.com>
2022-04-23 03:33:04 -0400

    refactor build.onLoad (caching, css, default)

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,52 +10,64 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        } // parse the index.js file, find any import/require/exports
+      build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+        // parse the index.js file, find any import/require/exports
         // args.path is updated
+      });
 
-        // Attempt to load that file
-        // Check if data is in cache
+      // Check if data is in cache
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // key: args.path
         // value: object {loader: .. , contents: .., resolveDir: ..}
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
 
-        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-        //   args.path
-        // );
-        // if (cachedResult) {
-        //   return cachedResult;
-        // }
-
-        // otherwise store result in cache
+      // Attempt to load css file
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path); // content of index.js file
-
-        // also handle css import
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
-        // including css file in js
+        // injecting css file inside js
         // import 'bulma/css/bulma.css'
         const escapedData = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\""')
           .replace(/'/g, "\\'"); // so we don't close out string in contents
-        const contents =
-          fileType === 'css'
-            ? `
-            const style = document.createElement('style');
-            style.innerText = '${escapedData}';
-            document.head.appendChild(style);
-          `
-            : data;
+        const contents = `
+              const style = document.createElement('style');
+              style.innerText = '${escapedData}';
+              document.head.appendChild(style);
+            `;
+
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        // store result in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // import react from 'react'
+        const { data, request } = await axios.get(args.path); // content of index.js file
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // store result in cache
         await fileCache.setItem(args.path, result);
 
         return result;
